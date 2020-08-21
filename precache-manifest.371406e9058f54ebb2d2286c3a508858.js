085e548326b006ea48ba2ff08d539128f53976be
edith estefaniq <edistefan416@gmail.com>
2020-08-21 11:30:13 -0500

    Updates

---
+++ precache-manifest.371406e9058f54ebb2d2286c3a508858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f83b635a9973aa3efd0acc75b19809",
+    "url": "/mi-app-react/%20./index.html"
+  },
+  {
+    "revision": "74bdb80b4e016f20d160",
+    "url": "/mi-app-react/%20./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b61657c882f704081a94",
+    "url": "/mi-app-react/%20./static/js/2.d008f8a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mi-app-react/%20./static/js/2.d008f8a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74bdb80b4e016f20d160",
+    "url": "/mi-app-react/%20./static/js/main.856da9b6.chunk.js"
+  },
+  {
+    "revision": "a41e9e2bb6864425da96",
+    "url": "/mi-app-react/%20./static/js/runtime-main.fcb04e0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mi-app-react/%20./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/componentes/micomponente1.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const micomponente1=() => {
-     return (  <h1>mi primer componente1 </h1>);
-    }
-export default micomponente1;
